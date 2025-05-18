2ddb9d9bcbab6da9af27b6a34c9792199086ae40
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-05-18 18:49:55 +0530

    Created a Sidebar component
    Added an Instagram text logo

--- Instagram Clone - React + Tailwind/src/App.jsx
+++ Instagram Clone - React + Tailwind/src/App.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <div className="text-3xl font-bold underline text-cyan-500">Instagram</div>
+    <>
+      <div className="flex h-screen bg-red-300">
+        <div className="bg-cyan-500 w-[20%]">
+          <Sidebar />
+        </div>
+        <div className="bg-blue-500 w-[50%]">Feed</div>
+        <div className="bg-pink-300 w-[30%]">Suggestions</div>
+      </div>
+    </>
   );
 }
 
---
+++ Instagram Clone - React + Tailwind/src/components/Sidebar.jsx
@@ -0,0 +1,28 @@
+function Sidebar() {
+  return (
+    <>
+      <div>
+        <img
+          src="src/assets/Instagram_logo.svg.png"
+          alt="insta text logo"
+          className="w-24"
+        ></img>
+        <div>Home</div>
+        <div>Search</div>
+        <div>Explore</div>
+        <div>Reels</div>
+        <div>Messages</div>
+        <div>Notifications</div>
+        <div>Create</div>
+        <div>Profile</div>
+      </div>
+
+      <div>
+        <div>Threads</div>
+        <div>More</div>
+      </div>
+    </>
+  );
+}
+
+export default Sidebar;
