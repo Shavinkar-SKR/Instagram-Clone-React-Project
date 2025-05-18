5313a3f5aa67e882e537ea4cd3a205053890b64c
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-05-18 17:00:30 +0530

    Configured Tailwind css
    Made adjustments to the vite.config.js, the required css file, and the jsx file to test  style

--- Instagram Clone - React + Tailwind/src/App.jsx
+++ Instagram Clone - React + Tailwind/src/App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="text-3xl font-bold underline text-cyan-500">Instagram</div>
+  );
 }
 
 export default App;
