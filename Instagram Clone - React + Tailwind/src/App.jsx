dc35ecf9be26171c25f28d7b0b7c8c1c264b91c1
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-05-18 20:47:02 +0530

    Inserted react-icons to the project
    This react-icons is flexible that includes icons from Material, Bootstrap, Heroicons, Font Awesome, etc.

--- Instagram Clone - React + Tailwind/src/App.jsx
+++ Instagram Clone - React + Tailwind/src/App.jsx
@@ -4,12 +4,12 @@ import Sidebar from "./components/Sidebar";
 function App() {
   return (
     <>
-      <div className="flex h-screen bg-red-300">
-        <div className="bg-cyan-500 w-[20%]">
+      <div className="flex h-screen">
+        <div className=" w-[20%]">
           <Sidebar />
         </div>
-        <div className="bg-blue-500 w-[50%]">Feed</div>
-        <div className="bg-pink-300 w-[30%]">Suggestions</div>
+        <div className="bg-cyan-500 w-[50%]">Feed</div>
+        <div className=" w-[30%]">Suggestions</div>
       </div>
     </>
   );
--- Instagram Clone - React + Tailwind/src/components/Sidebar.jsx
+++ Instagram Clone - React + Tailwind/src/components/Sidebar.jsx
@@ -1,27 +1,69 @@
+import { BiHome, BiSearch, BiCompass, BiHeart } from "react-icons/bi";
+import { BsPlayBtn, BsThreads } from "react-icons/bs";
+import { LuMessageCircleMore, LuCircleUserRound } from "react-icons/lu";
+import { FaRegSquarePlus } from "react-icons/fa6";
+import { TbMenu2 } from "react-icons/tb";
+
 function Sidebar() {
   return (
-    <>
-      <div>
+    <div className="m-6">
+      <div className="flex flex-col gap-y-3">
         <img
           src="src/assets/Instagram_logo.svg.png"
           alt="insta text logo"
           className="w-24"
         ></img>
-        <div>Home</div>
-        <div>Search</div>
-        <div>Explore</div>
-        <div>Reels</div>
-        <div>Messages</div>
-        <div>Notifications</div>
-        <div>Create</div>
-        <div>Profile</div>
+        <div className="flex items-center space-x-2">
+          <BiHome />
+          <span>Home</span>
+        </div>
+
+        <div className="flex items-center space-x-2">
+          <BiSearch />
+          <span>Search</span>
+        </div>
+        <div className="flex items-center space-x-2">
+          <BiCompass />
+          <span>Explore</span>
+        </div>
+
+        <div className="flex items-center space-x-2">
+          <BsPlayBtn />
+          <span>Reels</span>
+        </div>
+
+        <div className="flex items-center space-x-2">
+          <LuMessageCircleMore />
+          <span>Messages</span>
+        </div>
+
+        <div className="flex items-center space-x-2">
+          <BiHeart />
+          <span>Notifications</span>
+        </div>
+
+        <div className="flex items-center space-x-2">
+          <FaRegSquarePlus />
+          <span>Create</span>
+        </div>
+
+        <div className="flex items-center space-x-2">
+          <LuCircleUserRound />
+          <span>Profile</span>
+        </div>
       </div>
 
-      <div>
-        <div>Threads</div>
-        <div>More</div>
+      <div className="fixed bottom-7 flex flex-col gap-y-3">
+        <div className="flex items-center space-x-2">
+          <BsThreads />
+          <span>Threads</span>
+        </div>
+        <div className="flex items-center space-x-2">
+          <TbMenu2 />
+          <span>More</span>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
