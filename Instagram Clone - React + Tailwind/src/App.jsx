afdfb3ed18ef01703a19ab5314c6f8cd9f1684ff
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-05-19 18:55:46 +0530

    Created two components

--- Instagram Clone - React + Tailwind/src/App.jsx
+++ Instagram Clone - React + Tailwind/src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
+import Suggestions from "./components/Suggestions";
 
 function App() {
   return (
@@ -8,8 +10,12 @@ function App() {
         <div className=" w-[20%]">
           <Sidebar />
         </div>
-        <div className="bg-cyan-500 w-[50%]">Feed</div>
-        <div className=" w-[30%]">Suggestions</div>
+        <div className="bg-cyan-500 w-[50%]">
+          <Feed />
+        </div>
+        <div className=" w-[30%]">
+          <Suggestions />
+        </div>
       </div>
     </>
   );
