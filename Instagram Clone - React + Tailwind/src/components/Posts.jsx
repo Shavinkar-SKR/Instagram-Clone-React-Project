9f85c36791927a116fdf747125079cc1b6a2c627
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-05-19 19:25:26 +0530

    Updated to fetch the data in db.json

--- Instagram Clone - React + Tailwind/src/components/Posts.jsx
+++ Instagram Clone - React + Tailwind/src/components/Posts.jsx
@@ -1,6 +1,25 @@
 import React from "react";
 
+import { useState, useEffect } from "react";
+
 function Posts() {
+  const [posts, setPosts] = useState([]);
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/posts")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setPosts(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <>
       <div>Posts</div>
