f7a5d130ebe1fcb6fb1a8a888b07ac2c794ef48f
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-05-21 19:50:56 +0530

    Checked whether suggestion id-s are displayed

--- Instagram Clone - React + Tailwind/src/components/Suggestions.jsx
+++ Instagram Clone - React + Tailwind/src/components/Suggestions.jsx
@@ -15,28 +15,60 @@ function Suggestions() {
       .catch((err) => {
         console.log(err);
       });
+
+    fetch("http://localhost:3000/suggestions")
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        setSuggestions(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
 
   return (
-    <div className="">
+    <>
       {profile ? (
-        <div className="flex mt-9 justify-evenly">
-          <div className="flex">
-            <img
-              className="rounded-full h-15 w-15"
-              src={profile.profile_pic}
-              alt="Profile picture"
-            />
-            <div className="ml-2">{profile.username}</div>
+        <>
+          <div className="flex mt-9 justify-evenly">
+            <div className="flex">
+              <img
+                className="rounded-full h-15 w-15"
+                src={profile.profile_pic}
+                alt="Profile picture"
+              />
+              <div className="ml-2">{profile.username}</div>
+            </div>
+            <div className="flex items-center text-cyan-500">
+              <p>Switch</p>
+            </div>
           </div>
-          <div className="flex items-center text-cyan-500">
-            <p>Switch</p>
+
+          <div className="flex justify-evenly mt-5">
+            <p>Suggested for you</p>
+            <b>
+              <p className="text-sm">See all</p>
+            </b>
           </div>
-        </div>
+
+          <div>
+            {suggestions.length > 0 ? (
+              <div>
+                {suggestions.map((suggest) => (
+                  <div key={suggest.id}>{suggest.id}</div>
+                ))}
+              </div>
+            ) : (
+              <div>Loading Suggestions</div>
+            )}
+          </div>
+        </>
       ) : (
         <div>Loading</div>
       )}
-    </div>
+    </>
   );
 }
 
