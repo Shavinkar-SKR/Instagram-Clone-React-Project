0c27bf5de74315c2a81d0be66e472a79574e4dfc
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-05-22 22:50:14 +0530

    Used some elements from react-router-dom

--- Instagram Clone - React + Tailwind/src/components/ViewStory.jsx
+++ Instagram Clone - React + Tailwind/src/components/ViewStory.jsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function ViewStory() {
+  const [story, setStory] = useState(null);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/stories/${id}`)
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setStory(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <>
-      <div>Story</div>
+      {story ? (
+        <div>
+          {story.user.username}
+          <img src={story.image} alt="image" />
+        </div>
+      ) : (
+        <div>Loading</div>
+      )}
     </>
   );
 }
